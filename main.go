c6a67986b7d75b59baa8adede785658c5fe4a6a9
kami-zh <hiroki.zenigami@gmail.com>
2017-12-11 15:20:07 +0900

    Fix for golint

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 var captureStdout, captureStderr bool
 
+// CaptureStdout Capture `os.Stdout`
 func CaptureStdout(f func()) string {
 	captureStdout = true
 	defer func() {
@@ -16,6 +17,7 @@ func CaptureStdout(f func()) string {
 	return capture(f)
 }
 
+// CaptureStderr Capture `os.Stderr`
 func CaptureStderr(f func()) string {
 	captureStderr = true
 	defer func() {
@@ -24,6 +26,7 @@ func CaptureStderr(f func()) string {
 	return capture(f)
 }
 
+// CaptureOutput Capture `os.Stdout` and `os.Stderr`
 func CaptureOutput(f func()) string {
 	captureStdout = true
 	captureStderr = true
