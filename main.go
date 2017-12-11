54c02f97f7bc8f8f38fe5e9ad05e31249f737add
kami-zh <hiroki.zenigami@gmail.com>
2017-12-11 15:16:02 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package capturer
+
+import (
+	"bytes"
+	"io"
+	"os"
+)
+
+var captureStdout, captureStderr bool
+
+func CaptureStdout(f func()) string {
+	captureStdout = true
+	defer func() {
+		captureStdout = false
+	}()
+	return capture(f)
+}
+
+func CaptureStderr(f func()) string {
+	captureStderr = true
+	defer func() {
+		captureStderr = false
+	}()
+	return capture(f)
+}
+
+func CaptureOutput(f func()) string {
+	captureStdout = true
+	captureStderr = true
+	defer func() {
+		captureStdout = false
+		captureStderr = false
+	}()
+	return capture(f)
+}
+
+func capture(f func()) string {
+	r, w, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+
+	if captureStdout {
+		stdout := os.Stdout
+		os.Stdout = w
+		defer func() {
+			os.Stdout = stdout
+		}()
+	}
+
+	if captureStderr {
+		stderr := os.Stderr
+		os.Stderr = w
+		defer func() {
+			os.Stderr = stderr
+		}()
+	}
+
+	f()
+	w.Close()
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+
+	return buf.String()
+}
